f0f3a132f061a999287d874a596a8f8378f72d32
Criz <crizmo595@gmail.com>
2022-01-06 17:02:23 +0530

    Update horny.js

--- commands/affection/horny.js
+++ commands/affection/horny.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js')
 
 module.exports = {
- name: "horny",
- permissions: ["SEND_MESSAGES"],
- cooldown: 5,
- description: "horny command",
+    name: "horny",
+    permissions: ["SEND_MESSAGES"],
+    cooldown: 5,
+    description: "horny command",
 
- execute(client, message, args, Discord) {
- let member = message.mentions.users.first() || message.author
+    execute(client, message, args, Discord) {
+        let member = message.mentions.users.first() || message.author
 
- let rng = Math.floor(Math.random() * 101);
+        let rng = Math.floor(Math.random() * 101);
 
- const hornyembed = new Discord.MessageEmbed()
- .setTitle(`Horny`)
- .setDescription(`${member.username} is ` + rng + "% Horny ")
- .setColor("RANDOM")
+        const hornyembed = new Discord.MessageEmbed()
+            .setTitle(`Horny`)
+            .setDescription(`${member.username} is ` + rng + "% Horny 😏")
+            .setColor("RANDOM")
 
- message.channel.send({embeds: [hornyembed]})
- }
+        message.channel.send({ embeds: [hornyembed] })
+    }
 }
