e76d5c28832d06057724ab030f6fbc461ff182eb
Criz <crizmo595@gmail.com>
2022-01-08 19:44:40 +0530

    sleet

--- commands/genshin/character.js
+++ commands/genshin/character.js
@@ -16,51 +16,47 @@ module.exports = {
         .setTitle('Incorrect Usage <:paimon:927534293515911178>')
         .setURL('https://crizmo.github.io/elina/')
         .setDescription('To use the command correctly do \n `\ =gen-char {character_name} \`')
-        .addFields(
-            {name: 'Characters: ', value: "`\ albedo \` - `\ aloy \` - `\ amber \` - `\ arataki-itto \` - `\ ayaka \` - `\ barbara \` - `\ beidou \` - `\ bennett \` - `\ chongyun \` - `\ diluc \` - `\ diona \` -`\ eula \` - `\ fischl \` - `\ ganyu \` - `\ hu-tao \` - `\ jean \` - `\ kaeya \` - `\ kazuha \` - `\ keqing \` - `\ klee \` - `\ kokomi \` - `\ lisa \` - `\ mona \` - `\ ningguang \` - `\ noelle \` - `\ qiqi \` - `\ raiden \` - `\ razor \` - `\ rosaria \` - `\ sara \` - `\ sayu \` - `\ sucrose \` - `\ tartaglia \` - `\ thoma \` - `\ traveler-anemo `\ - \` traveler-electro \` - `\ traveler-geo `\ - \` venti `\ - \` xiangling \` -`\ xiao \` - `\ xingqiu \` - `\ xinyan \` - `\ yanfei \` - `\ yoimiya \` - `\ zhongli \`"},
-            {name: 'Usage: ', value: "`\ =gen-char {character_name} \`"},
-        )
         .setThumbnail("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1VIMxlog-aJsdR3Vk770oxvJx7baqzfS0vzp3Ujcr_KWMHj4gKc9Vh9jWojnp8WrwcU&usqp=CAU")
-        .setImage("https://mmos.com/wp-content/uploads/2021/04/genshin-impact-ps5-announce-key-art-banner.jpg")
+        .setImage("https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/c2904ed6cfa731edd18d8ffe285b6695.png")
         .setTimestamp();
 
         if (!name) return message.channel.send({ embeds: [errorEmbed]})
 
-            let Chars_Data = []
-            let D_character = await fetch(`https://api.genshin.dev/characters/${name}/`)
-            let I_character = `https://api.genshin.dev/characters/${name}/icon.png`
-            let Pic_character = `https://api.genshin.dev/characters/${name}/gacha-splash.png`
-            let data = await D_character.json();
+        let Chars_Data = []
+        let D_character = await fetch(`https://api.genshin.dev/characters/${name}/`)
+        let I_character = `https://api.genshin.dev/characters/${name}/icon.png`
+        let Pic_character = `https://api.genshin.dev/characters/${name}/gacha-splash.png`
+        let data = await D_character.json();
 
-            Chars_Data.push({
-                charname : data.name.toLocaleString(),
-                vision : data.vision.toLocaleString(),
-                nation : data.nation.toLocaleString(),
-                weapon : data.weapon.toLocaleString(),
-                affiliation : data.affiliation.toLocaleString(),
-                rarity : data.rarity.toLocaleString(),
-                constellation : data.constellation.toLocaleString(),
-                birthday : data.birthday.toLocaleString(),
-                description : data.description.toLocaleString(),
-                icon: I_character.toString(),
-                pic: Pic_character.toLocaleString()
-            });
+        Chars_Data.push({
+            charname : data.name.toLocaleString(),
+            vision : data.vision.toLocaleString(),
+            nation : data.nation.toLocaleString(),
+            weapon : data.weapon.toLocaleString(),
+            affiliation : data.affiliation.toLocaleString(),
+            rarity : data.rarity.toLocaleString(),
+            constellation : data.constellation.toLocaleString(),
+            birthday : data.birthday.toLocaleString(),
+            description : data.description.toLocaleString(),
+            icon: I_character.toString(),
+            pic: Pic_character.toLocaleString()
+        })
 
-            const charEmbed = new MessageEmbed()
-                .setTitle(`Genshin Impact - ${Chars_Data[0].charname}`) 
-                .setDescription(`${Chars_Data[0].description}`)
-                .addFields(
-                    { name: `Rarity`, value: `${Chars_Data[0].rarity} ⭐`, inline: true},
-                    { name: `Vision`, value: `${Chars_Data[0].vision}`, inline: true},
-                    { name: `Nation`, value: `${Chars_Data[0].nation}`, inline: true},
-                    { name: `Constellation`, value: `${Chars_Data[0].constellation}`, inline: true},
-                    { name: `Weapon`, value: `${Chars_Data[0].weapon}`, inline: true},
-                    { name: `Affiliation`, value: `${Chars_Data[0].affiliation}`, inline: true},
-                )
-                .setImage(`${Chars_Data[0].pic}`)
-                .setThumbnail(`${Chars_Data[0].icon}`)
-                .setTimestamp()
+        const charEmbed = new MessageEmbed()
+            .setTitle(`Genshin Impact - ${Chars_Data[0].charname}`) 
+            .setDescription(`${Chars_Data[0].description}`)
+            .addFields(
+                { name: `Rarity`, value: `${Chars_Data[0].rarity} ⭐`, inline: true},
+                { name: `Vision`, value: `${Chars_Data[0].vision}`, inline: true},
+                { name: `Nation`, value: `${Chars_Data[0].nation}`, inline: true},
+                { name: `Constellation`, value: `${Chars_Data[0].constellation}`, inline: true},
+                { name: `Weapon`, value: `${Chars_Data[0].weapon}`, inline: true},
+                { name: `Affiliation`, value: `${Chars_Data[0].affiliation}`, inline: true},
+            )
+            .setImage(`${Chars_Data[0].pic}`)
+            .setThumbnail(`${Chars_Data[0].icon}`)
+            .setTimestamp()
 
-            message.channel.send({ embeds: [charEmbed] });
-        }
+        message.channel.send({ embeds: [charEmbed] })
     }
+}
--- commands/genshin/genshin.js
+++ commands/genshin/genshin.js
@@ -1,11 +1,23 @@
+const fetch = require('node-fetch');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'genshin',
     permissions: ["SEND_MESSAGES"],
     description: "Embeds!",
     execute(client, message, args, Discord){
-        const newEmbed = new Discord.MessageEmbed()
-        .setTitle("Still working")
-        .setDescription('lol')
-        message.channel.send({embeds: [newEmbed]})
+
+        let description = "Genshin Impact is an open-world action RPG with “gacha” (we wll go over that later on) mechanics. It is developed and published by Chinese studio miHoYo. In it, players control a number of party members, each with different abilities, weapons, gear, and personalities. The combat plays out in real time, allowing players to utilize ranged, melee, and elemental attacks against a wide array of enemies across the game(s) open world and dungeons."
+        let image = "https://thumbs.gfycat.com/AltruisticAggressiveAddax-size_restricted.gif"
+        let thumb = "https://media.discordapp.net/attachments/928257362106654720/929376683956510790/artworks-w6XYmsnp5PGKGQtL-tDQZAQ-t500x500.png?width=495&height=495"
+
+        const genEmbed = new MessageEmbed()
+        .setTitle(`Genshin Impact`) 
+        .setDescription(`${description}`)
+        .setImage(`${image}`)
+        .setThumbnail(`${thumb}`)
+        .setTimestamp()
+
+        message.channel.send({ embeds: [genEmbed] })
     }
 }
