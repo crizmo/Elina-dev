6b26ae0ba2506b11cc1582978d682819605cb0e1
Criz <crizmo595@gmail.com>
2022-01-13 23:26:56 +0530

    Update elements.js

--- commands/genshin/elements.js
+++ commands/genshin/elements.js
@@ -23,7 +23,6 @@ module.exports = {
         if (!name) return message.channel.send({ embeds: [errorEmbed]})
 
         let Elem_Data = []
-        let Elemodd_Data = []
         let D_elements = await fetch(`https://api.genshin.dev/elements/${name}/`)
         let I_elements = `https://api.genshin.dev/elements/${name}/icon.png`
         let data = await D_elements.json();
@@ -40,37 +39,89 @@ module.exports = {
                     reacname2 : data.reactions[1].name.toLocaleString(),
                     reacelem2 : data.reactions[1].elements.toLocaleString(),
                     reacdesc2 : data.reactions[1].description.toLocaleString(),
-                // rec 2 end 
-                // rec 2
+                // // rec 2 end 
+                // // rec 3
                     reacname3 : data.reactions[2].name.toLocaleString(),
                     reacelem3 : data.reactions[2].elements.toLocaleString(),
                     reacdesc3 : data.reactions[2].description.toLocaleString(),
-                // rec 2 end 
+                // rec 3 end 
+                // // rec 3
+                    reacname4 : data.reactions[3].name.toLocaleString(),
+                    reacelem4 : data.reactions[3].elements.toLocaleString(),
+                    reacdesc4 : data.reactions[3].description.toLocaleString(),
+                // rec 3 end
             // reaction end
             icon : I_elements.toString(),
         })
 
-        const elEmbed = new MessageEmbed()
-            .setTitle(`Element info - ${Elem_Data[0].elname}`) 
-            .setDescription("Genshin impact element info")
-            .addFields(
-                // rec 1
-                { name: `Reaction 1`, value: `${Elem_Data[0].reacname1}`, inline: true},
-                { name: `Element combo`, value: `${Elem_Data[0].reacelem1}`, inline: true},
-                { name: `Description`, value: `${Elem_Data[0].reacdesc1}`},
-                // rec 2
-                { name: `Reaction 2`, value: `${Elem_Data[0].reacname2}`, inline: true},
-                { name: `Element combo`, value: `${Elem_Data[0].reacelem2}`, inline: true},
-                { name: `Description`, value: `${Elem_Data[0].reacdesc2}`},
-                // rec 3
-                { name: `Reaction 3`, value: `${Elem_Data[0].reacname3}`, inline: true},
-                { name: `Element combo`, value: `${Elem_Data[0].reacelem3}`, inline: true},
-                { name: `Description`, value: `${Elem_Data[0].reacdesc3}`},
-            )
-            .setThumbnail(`${Elem_Data[0].icon}`)
-            .setTimestamp()
+        if(data.reactions[0] && !data.reactions[1] && !data.reactions[2] && !data.reactions[3]){
+            const elEmbed = new MessageEmbed()
+                .setTitle(`Element info - ${Elem_Data[0].elname}`) 
+                .setDescription("Genshin impact element info")
+                .addFields(
+                    // rec 1
+                    { name: `Reaction 1`, value: `${Elem_Data[0].reacname1}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem1}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc1}`},
+                )
+                .setThumbnail(`${Elem_Data[0].icon}`)
+                .setTimestamp()
+    
+            message.channel.send({ embeds: [elEmbed] }) 
+                }
 
-        message.channel.send({ embeds: [elEmbed] })
-            
+        else if(data.reactions[0] && data.reactions[1] && data.reactions[2] && !data.reactions[3]){
+            const elEmbed = new MessageEmbed()
+                .setTitle(`Element info - ${Elem_Data[0].elname}`) 
+                .setDescription("Genshin impact element info")
+                .addFields(
+                    // rec 1
+                    { name: `Reaction 1`, value: `${Elem_Data[0].reacname1}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem1}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc1}`},
+                    // rec 2
+                    { name: `Reaction 2`, value: `${Elem_Data[0].reacname2}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem2}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc2}`},
+                    // rec 3
+                    { name: `Reaction 3`, value: `${Elem_Data[0].reacname3}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem3}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc3}`},
+                )
+                .setThumbnail(`${Elem_Data[0].icon}`)
+                .setTimestamp()
+
+            message.channel.send({ embeds: [elEmbed] }) 
+            }
+            else if(data.reactions[0] && data.reactions[1] && data.reactions[2] && data.reactions[3]){
+                const elEmbed = new MessageEmbed()
+                .setTitle(`Element info - ${Elem_Data[0].elname}`) 
+                .setDescription("Genshin impact element info")
+                .addFields(
+                    // rec 1
+                    { name: `Reaction 1`, value: `${Elem_Data[0].reacname1}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem1}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc1}`},
+                    // rec 2
+                    { name: `Reaction 2`, value: `${Elem_Data[0].reacname2}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem2}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc2}`},
+                    // rec 3
+                    { name: `Reaction 3`, value: `${Elem_Data[0].reacname3}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem3}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc3}`},
+                    // rec 3
+                    { name: `Reaction 4`, value: `${Elem_Data[0].reacname4}`, inline: true},
+                    { name: `Element combo`, value: `${Elem_Data[0].reacelem4}`, inline: true},
+                    { name: `Description`, value: `${Elem_Data[0].reacdesc4}`},
+                )
+                .setThumbnail(`${Elem_Data[0].icon}`)
+                .setTimestamp()
+
+                message.channel.send({ embeds: [elEmbed] })
+            }
+            else {
+                message.channel.send("hi")
+            }
     }
 }
