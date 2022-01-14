b2cfc465b0906c2a1023cd9af6fb687f311d8696
Criz <crizmo595@gmail.com>
2022-01-14 14:34:39 +0530

    More new cmds

---
+++ commands/genshin/nations.js
@@ -0,0 +1,53 @@
+const fetch = require('node-fetch');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "genshin-nations",
+    aliases: ["gen-nat", "gen-nations", "genshin-nat"],
+    cooldown: 5,
+    permissions: ["SEND_MESSAGES"],
+    description: "Genshin info & stats command",
+    async execute(client, message, args, Discord) {
+
+        let name = args[0]
+
+        const errorEmbed = new Discord.MessageEmbed()
+        .setColor('#00FFFF')
+        .setTitle('Incorrect Usage <:paimon:927534293515911178>')
+        .setURL('https://crizmo.github.io/elina/')
+        .setDescription('To use the command correctly do \n `\ =gen-nat {nation_name} \`')
+        .setThumbnail("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1VIMxlog-aJsdR3Vk770oxvJx7baqzfS0vzp3Ujcr_KWMHj4gKc9Vh9jWojnp8WrwcU&usqp=CAU")
+        .setImage("https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/c2904ed6cfa731edd18d8ffe285b6695.png")
+        .setTimestamp();
+
+        if (!name) return message.channel.send({ embeds: [errorEmbed]})
+
+        let Nat_Data = []
+        let D_nation = await fetch(`https://api.genshin.dev/nations/${name}/`)
+        let I_nation = `https://api.genshin.dev/nations/${name}/icon.png`
+        let data = await D_nation.json();
+
+        if(!data.name) return message.channel.send("wrong name");
+
+        Nat_Data.push({
+            natname : data.name.toLocaleString(),
+            element : data.element.toLocaleString(),
+            archon : data.archon.toLocaleString(),
+            main : data.controllingEntity.toLocaleString(),
+            icon: I_nation.toString(),
+        })
+
+        const natEmbed = new MessageEmbed()
+            .setTitle(`Genshin Impact - ${Nat_Data[0].natname}`) 
+            .setDescription("Genshin impact nation info")
+            .addFields(
+                { name: `Element`, value: `${Nat_Data[0].element}`, inline: true},
+                { name: `Archon`, value: `${Nat_Data[0].archon}`, inline: true},
+                { name: `Main`, value: `${Nat_Data[0].main}`, inline: true},
+            )
+            .setThumbnail(`${Nat_Data[0].icon}`)
+            .setTimestamp()
+
+        message.channel.send({ embeds: [natEmbed] })
+    }
+}
---
+++ commands/genshin/weapons.js
@@ -0,0 +1,61 @@
+const fetch = require('node-fetch');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "genshin-weapon",
+    aliases: ["gen-wp", "gen-weapon", "genshin-wp"],
+    cooldown: 5,
+    permissions: ["SEND_MESSAGES"],
+    description: "Genshin info & stats command",
+    async execute(client, message, args, Discord) {
+
+        let name = args.join(' ');
+
+        const errorEmbed = new Discord.MessageEmbed()
+        .setColor('#00FFFF')
+        .setTitle('Incorrect Usage <:paimon:927534293515911178>')
+        .setURL('https://crizmo.github.io/elina/')
+        .setDescription('To use the command correctly do \n `\ =gen-wp {weapon_name} \`\nIf the weapon name has space then `\ =gen-wp aquila-favonia \`')
+        .setThumbnail("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU1VIMxlog-aJsdR3Vk770oxvJx7baqzfS0vzp3Ujcr_KWMHj4gKc9Vh9jWojnp8WrwcU&usqp=CAU")
+        .setImage("https://cdn2.steamgriddb.com/file/sgdb-cdn/grid/c2904ed6cfa731edd18d8ffe285b6695.png")
+        .setTimestamp();
+
+        if (!name) return message.channel.send({ embeds: [errorEmbed]})
+
+        let Wp_Data = []
+        let D_weapon = await fetch(`https://api.genshin.dev/weapons/${name}/`)
+        let I_weapon = `https://api.genshin.dev/weapons/${name}/icon.png`
+        let data = await D_weapon.json();
+
+        if(!data.name) return message.channel.send("wrong name");
+
+        Wp_Data.push({
+            wpname : data.name.toLocaleString(),
+            type : data.type.toLocaleString(),
+            rarity : data.rarity.toLocaleString(),
+            baseAttack : data.baseAttack.toLocaleString(),
+            subStats : data.subStat.toLocaleString(),
+            passiveName : data.passiveName.toLocaleString(),
+            passiveDesc : data.passiveDesc.toLocaleString(),
+            location : data.location.toLocaleString(),
+            icon: I_weapon.toString(),
+        })
+
+        const wpEmbed = new MessageEmbed()
+            .setTitle(`Genshin Impact - ${Wp_Data[0].wpname}`) 
+            .setDescription("Genshin impact nation info")
+            .addFields(
+                { name: `Type`, value: `${Wp_Data[0].type}`, inline: true},
+                { name: `Rarity`, value: `${Wp_Data[0].rarity} ⭐`, inline: true},
+                { name: `Base Atk`, value: `${Wp_Data[0].baseAttack}`, inline: true},
+                { name: `Sub Stats`, value: `${Wp_Data[0].subStats}`, inline: true},
+                { name: `Passive`, value: `${Wp_Data[0].passiveName}`, inline: true},
+                { name: `Location`, value: `${Wp_Data[0].location}`, inline: true},
+                { name: `Description`, value: `${Wp_Data[0].passiveDesc}`},
+            )
+            .setThumbnail(`${Wp_Data[0].icon}`)
+            .setTimestamp()
+
+        message.channel.send({ embeds: [wpEmbed] })
+    }
+}
