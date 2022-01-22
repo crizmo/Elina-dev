a97686ebc86eea1f2d0926a2b0d835e2275c12de
Criz <crizmo595@gmail.com>
2022-01-22 21:58:25 +0530

    Update testing.js

--- commands/genshin/testing.js
+++ commands/genshin/testing.js
@@ -4,14 +4,29 @@ const tey = new Tey.default(process.env.TEYTOKEN);
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "ambertest",
+    name: "tc",
     cooldown: 5,
     permissions: ["SEND_MESSAGES"],
     description: "Genshin info & stats command",
     async execute(client, message, args, Discord) {
-        tey.getCharacter('Amber').then((data) => {
-            console.log(data);
-            //Expected, Amber stats
-          });
-    }
+
+        let name = args[0]
+
+        const rlname = name.charAt(0).toUpperCase() + name.slice(1);
+        
+        let Char_Data = []
+        let data = await tey.getCharacter(`${rlname}`);
+
+            Char_Data.push({
+                charname : data.name.toLocaleString(),
+                description : data.id.toLocaleString(),
+            })
+
+        const charEmbed = new MessageEmbed()
+        .setTitle(`${Char_Data[0].charname}`)
+        .setDescription(`${Char_Data[0].description}`)
+
+        message.channel.send({ embeds: [charEmbed] })
+
+        }    
 }
