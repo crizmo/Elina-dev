e9bca71ad4e987507703b2a5d1270ce3a2724dde
Criz <crizmo595@gmail.com>
2022-02-01 18:14:52 +0530

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,10 @@
+# For hosting on replit
+
+# { pkgs }: {
+#   deps = [
+#     pkgs.nodejs-16_x
+#       pkgs.nodePackages.typescript-language-server
+#       pkgs.nodePackages.yarn
+#       pkgs.replitPackages.jest
+#   ];
+# }
