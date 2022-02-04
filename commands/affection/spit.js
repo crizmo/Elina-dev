e89f1231242468ca8075d2249a07516a43dbd08b
Criz <crizmo595@gmail.com>
2022-02-04 08:34:16 +0530

    Update spit.js

--- commands/affection/spit.js
+++ commands/affection/spit.js
@@ -18,14 +18,14 @@ module.exports = {
                 .setTitle(`${message.author.username} spat on ${member.displayName}`)
                 .setTimestamp()
                 .setImage(spitLinks[randomNum])
-                .setDescription('<:Elmo_Fire:805428510692343848> you <:cheese:880525560714002512> me <:flushedcool:808923862916923444>')
+                .setDescription('Ew why !!')
             message.channel.send({embeds: [firstEmbed]})
         }else{
             var firstEmbed = new Discord.MessageEmbed()
 	            .setColor("RANDOM")
                 .setTimestamp()
                 .setImage(spitLinks[randomNum])
-                .setDescription('<:Elmo_Fire:805428510692343848> <:cheese:880525560714002512> <:flushedcool:808923862916923444>')
+                .setDescription('Uhm...')
             message.channel.send({embeds: [firstEmbed]})
 
         }
