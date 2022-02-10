6ca6cd9376a5fe21a0aff984ea67a6f7088b646f
Criz <crizmo595@gmail.com>
2022-02-10 21:58:19 +0530

    glb for slash

--- src/slash/economy/global_Lb.js
+++ src/slash/economy/global_Lb.js
@@ -6,25 +6,39 @@ const cs = new CurrencySystem;
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('glb')
-		.setDescription('To check global leaderboard'),
+		.setDescription('To check global leaderboard')
+        .addIntegerOption(option => option.setName('number').setDescription('Enter an integer')),
+
 	async execute(interaction, client) {
+
+        
+        const amount = interaction.options.getInteger('number');
+
+        if (amount > 20) return interaction.reply("You can only view max top 20!");
 
         let data = await cs.globalLeaderboard();
         if (data.length < 1) return interaction.reply("Nobody's in Global leaderboard yet.");
         const msg = new MessageEmbed();
         let pos = 0;
-        // To make global leaderboard show members
-        // of current guild only, un comment the next line.
-    
-        // data = data.filter(e => message.guild.members.cache.has(e.userID));
 
-        // This is to get First 10 Users )
-        data.slice(0, 10).map(e => {
-            pos++
-            if (!client.users.cache.get(e.userID)) return;
-            msg.addField(`${pos} - **${client.users.cache.get(e.userID).username}**`, `Wallet: **${e.wallet}** - Bank: **${e.bank}**`, true);
-        });
+        if (amount) {
+            data.slice(0, amount).map(e => {
+                pos++
+                if (!client.users.cache.get(e.userID)) return;
+                const total = e.wallet + e.bank;
+                msg.addField(`${pos} - **${client.users.cache.get(e.userID).username}**`, `Wallet: **${e.wallet}** \n Bank: **${e.bank}** \n Total: **${total}**`, true);
+            });
 
-        interaction.reply({embeds: [msg]}).catch();
+            interaction.reply({embeds: [msg]}).catch();
+        } else {
+            data.slice(0, 10).map(e => {
+                pos++
+                if (!client.users.cache.get(e.userID)) return;
+                const total = e.wallet + e.bank;
+                msg.addField(`${pos} - **${client.users.cache.get(e.userID).username}**`, `Wallet: **${e.wallet}** \n Bank: **${e.bank}** \n Total: **${total}**`, true);
+            });
+
+            interaction.reply({embeds: [msg]}).catch();
+        }
 	},
 };
