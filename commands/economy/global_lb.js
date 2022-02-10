ed89009c08e6a4cedefe5c89081683a64adf44ed
Criz <crizmo595@gmail.com>
2022-02-10 20:51:57 +0530

    Update global_lb.js

--- commands/economy/global_lb.js
+++ commands/economy/global_lb.js
@@ -11,17 +11,34 @@ module.exports = {
     description: "Leaderboard command",
 
     async execute(client, message, args, Discord) {
+
+        const amount = args[0];
+
+        if (amount > 20) return message.channel.send("You can only view max top 20!");
+
         let data = await cs.globalLeaderboard();
         if (data.length < 1) return message.channel.send("Nobody's in Global leaderboard yet.");
         const msg = new MessageEmbed();
         let pos = 0;
-        data.slice(0, 10).map(e => {
-            pos++
-            if (!client.users.cache.get(e.userID)) return;
-            const total = e.wallet + e.bank;
-            msg.addField(`${pos} - **${client.users.cache.get(e.userID).username}**`, `Wallet: **${e.wallet}** \n Bank: **${e.bank}** \n Total: **${total}**`, true);
-        });
 
-        message.channel.send({embeds: [msg]}).catch();
+        if (amount) {
+            data.slice(0, amount).map(e => {
+                pos++
+                if (!client.users.cache.get(e.userID)) return;
+                const total = e.wallet + e.bank;
+                msg.addField(`${pos} - **${client.users.cache.get(e.userID).username}**`, `Wallet: **${e.wallet}** \n Bank: **${e.bank}** \n Total: **${total}**`, true);
+            });
+
+            message.channel.send({embeds: [msg]}).catch();
+        } else {
+            data.slice(0, 10).map(e => {
+                pos++
+                if (!client.users.cache.get(e.userID)) return;
+                const total = e.wallet + e.bank;
+                msg.addField(`${pos} - **${client.users.cache.get(e.userID).username}**`, `Wallet: **${e.wallet}** \n Bank: **${e.bank}** \n Total: **${total}**`, true);
+            });
+
+            message.channel.send({embeds: [msg]}).catch();
+        }
     }
 }
