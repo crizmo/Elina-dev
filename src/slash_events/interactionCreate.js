efd83542fdd8410e357661a44228846495b10bd5
Criz <crizmo595@gmail.com>
2022-02-25 16:58:13 +0530

    Update interactionCreate.js

--- src/slash_events/interactionCreate.js
+++ src/slash_events/interactionCreate.js
@@ -12,38 +12,38 @@ module.exports = {
 
 		try {
 
-			if(slashcommand.permissions && slashcommand.permissions.length > 0){
-				if(!interaction.member.permissions.has(slashcommand.permissions)) return await interaction.reply({ content: "You do not have permission to use this command."});
+			if (slashcommand.permissions && slashcommand.permissions.length > 0) {
+				if (!interaction.member.permissions.has(slashcommand.permissions)) return await interaction.reply({ content: "You do not have permission to use this command." });
 			}
 
 			if (!cooldowns.has(slashcommand.commandName)) {
 
 				cooldowns.set(slashcommand.commandName, new Discord.Collection())
-	
+
 			}
-	
+
 			const currentTime = Date.now()
 			const timeStamps = cooldowns.get(slashcommand.commandName)
 			const cooldownAmount = (slashcommand.cooldown) * 1000
-	
+
 			if (timeStamps.has(interaction.user.id)) {
-	
+
 				const expTime = timeStamps.get(interaction.user.id) + cooldownAmount
-	
+
 				if (currentTime < expTime) {
-	
+
 					const timeLeft = (expTime - currentTime) / 1000
-	
+
 					const tmotEmbed = new Discord.MessageEmbed()
 						.setColor("#F886F6")
 						.setDescription(`❌ - Please wait \`${timeLeft.toFixed(1)}\` more seconds before using \`${interaction.commandName}\`!`)
-	
+
 					return interaction.reply({ embeds: [tmotEmbed] })
 				}
 			}
-	
+
 			timeStamps.set(interaction.user.id, currentTime)
-	
+
 			setTimeout(() => {
 				timeStamps.delete(interaction.user.id)
 			}, cooldownAmount)
@@ -52,19 +52,19 @@ module.exports = {
 		} catch (error) {
 			console.error(error);
 			const client = interaction.client;
-			async (reason , p , origin) => {
-			const err1embed = new Discord.MessageEmbed()
-			.setTitle('Error Occured')
-			.setColor('RANDOM')
-			.setDescription('```js\n' + reason.stack + '```');
-			client.channels.cache.get('917399374806536252').send({embeds: [err1embed]})
+			async (reason, p, origin) => {
+				const err1embed = new Discord.MessageEmbed()
+					.setTitle('Error Occured')
+					.setColor('RANDOM')
+					.setDescription('```js\n' + reason.stack + '```');
+				client.channels.cache.get('917399374806536252').send({ embeds: [err1embed] })
 			}
-			
+
 			const err2embed = new Discord.MessageEmbed()
-			.setTitle('Error Occured')
-			.setColor('RANDOM')
-			.setDescription('```js\n' + error.stack + '```');
-			client.channels.cache.get('917399374806536252').send({embeds: [err2embed]})
+				.setTitle('Error Occured')
+				.setColor('RANDOM')
+				.setDescription('```js\n' + error.stack + '```');
+			client.channels.cache.get('917399374806536252').send({ embeds: [err2embed] })
 
 			await interaction.reply({ content: 'There was an error while executing this command! \n|| Missing Channel/Server Permissions to send reply!', ephemeral: true });
 		}
