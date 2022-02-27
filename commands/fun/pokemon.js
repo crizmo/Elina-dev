6d106dad3fa2329f1f909036fe24b8a2c0c1f985
Criz <crizmo595@gmail.com>
2022-02-27 22:55:33 +0530

    Update pokemon.js

--- commands/fun/pokemon.js
+++ commands/fun/pokemon.js
@@ -4,25 +4,25 @@ const { version } = require('discord.js');
 
 module.exports = {
     name: "gtp",
-    aliases: ["poekmon"],
+    aliases: ["pokemon"],
     cooldown: 2,
     permissions: ["SEND_MESSAGES"],
     description: "would you rather",
-    async execute(client, message, args, Discord, cmd){
+    async execute(client, message, args, Discord, cmd) {
 
         new GuessThePokemon({
-        message: message,
-        slash_command: false,
-        embed: {
-            title: 'Who\'s This Pokemon?',
-            footer: 'You have only 1 chance',
-            color: '#5865F2',
-        },
-        time: 60000,
-        thinkMessage: '**Thinking...**',
-        winMessage: 'Nice! The pokemon was **{pokemon}**',
-        stopMessage: 'Better luck next time! It was a **{pokemon}**',
-        incorrectMessage: 'Nope! The pokemon was **{pokemon}**',
+            message: message,
+            slash_command: false,
+            embed: {
+                title: 'Who\'s This Pokemon?',
+                footer: 'You have only 1 chance',
+                color: '#5865F2',
+            },
+            time: 60000,
+            thinkMessage: '**Thinking...**',
+            winMessage: 'Nice! The pokemon was **{pokemon}**',
+            stopMessage: 'Better luck next time! It was a **{pokemon}**',
+            incorrectMessage: 'Nope! The pokemon was **{pokemon}**',
         }).startGame();
     }
 }
