ddde33a5ba46a77397f816d243e2c84e7dbead2e
Kurizu <83665497+crizmo@users.noreply.github.com>
2022-08-28 18:26:00 +0530

    🔨 : opti

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const server = express();
 server.all('/', (req, res) => {
 
     res.setHeader('Content-Type', 'text/html');
-
     res.write('<link href="https://fonts.googleapis.com/css?family=Roboto Condensed" rel="stylesheet"> <style> body {font-family: "Roboto Condensed";font-size: 22px;} <p>Hosting Active</p>');
-
     res.end();
 
 })
