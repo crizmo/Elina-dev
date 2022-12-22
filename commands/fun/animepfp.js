ae135fcbbf2040d649ec4f264bc9fecf015c9bac
Kurizu <83665497+crizmo@users.noreply.github.com>
2022-12-22 17:02:34 +0530

    📦 : pkg changes

--- commands/fun/animepfp.js
+++ commands/fun/animepfp.js
@@ -6,11 +6,11 @@ module.exports = {
     cooldown: 2,
     permissions: ["SEND_MESSAGES"],
     description: "Get random anime image / pfp",
-    execute(client, message, args, Discord, cmd) {
-        // const anime = anyanime.anime();         // Returns a random anime image
+    async execute(client, message, args, Discord, cmd) {
+        // const anime = await anyanime.anime();         // Returns a random anime image
         // message.channel.send(anime);
 
-        const anime = anyanime.anime();
+        const anime = await anyanime.anime();
         const embed = new Discord.MessageEmbed().setImage(anime);      // Returns a random anime image but inside an embed
         message.channel.send({ embeds: [embed] });
     }
